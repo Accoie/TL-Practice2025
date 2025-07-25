d95c1095c7b323534d6aad2b647b2f95b551296e
Accoie <sypherjensqr@gmail.com>
2025-07-25 14:23:20 +0300

    Corrected task

---
+++ Casino/Casino/CasinoOperationHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Casino;
+using CasinoOperation;
+
+public class CasinoOperationHandler
+{
+    public static void HandleOperation( CasinoGame casino, Operation operation )
+    {
+        switch ( operation )
+        {
+            case Operation.Initial:
+                return;
+            case Operation.Play:
+                {
+                    casino.PlayGame();
+                    break;
+                }
+            case Operation.CheckBalance:
+                {
+                    CasinoPrintInfo.PrintBalance( casino.Balance );
+                    break;
+                }
+            case Operation.Exit:
+                {
+                    Console.WriteLine( "Goodbye!!!" );
+                    break;
+                }
+            default:
+                throw new Exception( $"Unsupported operation passed {operation}." );
+        }
+    }
+}
---
+++ Casino/Casino/ReadUserInput.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CasinoOperation;
+using System.Globalization;
+
+namespace Casino;
+public class ReadUserInput
+{
+    public static double? ReadBet()
+    {
+        Console.Write( "Please, write bet: " );
+        string betStr = Console.ReadLine() ?? string.Empty;
+
+        bool isBetParsed = double.TryParse( betStr,
+            new CultureInfo( "en-US" ), 
+            out double bet );
+
+        if ( !isBetParsed )
+        {
+            Console.WriteLine( "Invalid bet! Example: 3.3" );
+            return null;
+        }
+        if ( bet <= 1 )
+        {
+            Console.WriteLine( "Bet must be more than 1!" );
+            return null;
+        }
+
+        return bet;
+    }
+
+    public static int? ReadBalance()
+    {
+        Console.Write( "Please enter amount of money you'd like to lose: " );
+
+        string balanceStr = Console.ReadLine() ?? string.Empty;
+
+        bool isBalanceParsed = int.TryParse( balanceStr, out int balance );
+
+        if ( !isBalanceParsed )
+        {
+            Console.WriteLine( $"Invalid balance value entered: {balanceStr}" );
+            return null;
+        }
+        if ( balance <= 0 )
+        {
+            Console.WriteLine( "Balance cannot be less than 1" );
+            return null;
+        }
+
+        return balance;
+    }
+
+    public static Operation? ReadOperation()
+    {
+        string operationStr = Console.ReadLine() ?? string.Empty;
+        bool isParsed = Enum.TryParse( operationStr, out Operation operation );
+
+        return isParsed ? operation : null;
+    }
+}
