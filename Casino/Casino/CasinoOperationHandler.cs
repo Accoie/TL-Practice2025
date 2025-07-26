35ed9868c1a96b46ac44d5cc2065c8b70ce7a999
Accoie <159988369+Accoie@users.noreply.github.com>
2025-07-26 17:35:26 +0300

    Update CasinoOperationHandler.cs

--- Casino/Casino/CasinoOperationHandler.cs
+++ Casino/Casino/CasinoOperationHandler.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CasinoOperation;
 
 namespace Casino;
+
 public class CasinoOperationHandler
 {
     public static void HandleOperation( CasinoGame casino, Operation operation )
